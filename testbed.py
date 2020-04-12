5c7980e3b259c8856fbb06aeea05b042649bf9ac
kalkaran <neal.norman@gmail.com>
2020-04-12 12:39:01 +0200

    Starting find hulls

--- testbed.py
+++ testbed.py
@@ -36,7 +36,24 @@ Various test images paths:
     squash = cv2.imread(squashpath)
     
 """
+data_dir="data" # Directory that will contain all kinds of data (the data we download and the data we generate)
 
+if not os.path.isdir(data_dir):
+    os.makedirs(data_dir)
+
+card_suits=['s','h','d','c']
+card_values=['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+
+# Pickle file containing the background images from the DTD
+backgrounds_pck_fn=data_dir+"/backgrounds.pck"
+
+# Pickle file containing the card images
+cards_pck_fn=data_dir+"/cards.pck"
+
+
+# imgW,imgH: dimensions of the generated dataset images
+imgW=720
+imgH=720
 
 cv2_resource_path="./venv/lib/python3.7/site-packages/cv2/data/"
 
@@ -647,103 +664,145 @@ def augment_function(img):
 
 #find hulls:
 #this function is taken from someone else
+#adding some variables to from the jupyter notebook to fix function
 
-# def findHull(img, corner=refCornerHL, debug="no"):
-#     """
-#         Find in the zone 'corner' of image 'img' and return, the convex hull delimiting
-#         the value and suit symbols
-#         'corner' (shape (4,2)) is an array of 4 points delimiting a rectangular zone,
-#         takes one of the 2 possible values : refCornerHL or refCornerLR
-#         debug=
-#     """
-#
-#     kernel = np.ones((3, 3), np.uint8)
-#     corner = corner.astype(np.int)
-#
-#     # We will focus on the zone of 'img' delimited by 'corner'
-#     x1 = int(corner[0][0])
-#     y1 = int(corner[0][1])
-#     x2 = int(corner[2][0])
-#     y2 = int(corner[2][1])
-#     w = x2 - x1
-#     h = y2 - y1
-#     zone = img[y1:y2, x1:x2].copy()
-#     im
-#
-#     strange_cnt = np.zeros_like(zone)
-#     gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
-#     thld = cv2.Canny(gray, 30, 200)
-#     thld = cv2.dilate(thld, kernel, iterations=1)
-#     if debug != "no": cv2.imshow("thld", thld)
-#
-#     # Find the contours
-#     contours, _ = cv2.findContours(thld.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-#     min_area = 30  # We will reject contours with small area. TWEAK, 'zoom' dependant
-#     min_solidity = 0.3  # Reject contours with a low solidity. TWEAK
-#
-#     concat_contour = None  # We will aggregate in 'concat_contour' the contours that we want to keep
-#
-#     ok = True
-#     for c in contours:
-#         area = cv2.contourArea(c)
-#
-#         hull = cv2.convexHull(c)
-#         hull_area = cv2.contourArea(hull)
-#         solidity = float(area) / hull_area
-#         # Determine the center of gravity (cx,cy) of the contour
-#         M = cv2.moments(c)
-#         cx = int(M['m10'] / M['m00'])
-#         cy = int(M['m01'] / M['m00'])
-#         #  abs(w/2-cx)<w*0.3 and abs(h/2-cy)<h*0.4 : TWEAK, the idea here is to keep only the contours which are closed to the center of the zone
-#         if area >= min_area and abs(w / 2 - cx) < w * 0.3 and abs(h / 2 - cy) < h * 0.4 and solidity > min_solidity:
-#             if debug != "no":
-#                 cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
-#             if concat_contour is None:
-#                 concat_contour = c
-#             else:
-#                 concat_contour = np.concatenate((concat_contour, c))
-#         if debug != "no" and solidity <= min_solidity:
-#             print("Solidity", solidity)
-#             cv2.drawContours(strange_cnt, [c], 0, 255, 2)
-#             cv2.imshow("Strange contours", strange_cnt)
-#
-#     if concat_contour is not None:
-#         # At this point, we suppose that 'concat_contour' contains only the contours corresponding the value and suit symbols
-#         # We can now determine the hull
-#         hull = cv2.convexHull(concat_contour)
-#         hull_area = cv2.contourArea(hull)
-#         # If the area of the hull is to small or too big, there may be a problem
-#         min_hull_area = 940  # TWEAK, deck and 'zoom' dependant
-#         max_hull_area = 2120  # TWEAK, deck and 'zoom' dependant
-#         if hull_area < min_hull_area or hull_area > max_hull_area:
-#             ok = False
-#             if debug != "no":
-#                 print("Hull area=", hull_area, "too large or too small")
-#         # So far, the coordinates of the hull are relative to 'zone'
-#         # We need the coordinates relative to the image -> 'hull_in_img'
-#         hull_in_img = hull + corner[0]
-#
-#     else:
-#         ok = False
-#
-#     if debug != "no":
-#         if concat_contour is not None:
-#             cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
-#             cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
-#         cv2.imshow("Zone", zone)
-#         cv2.imshow("Image", img)
-#         if ok and debug != "pause_always":
-#             key = cv2.waitKey(1)
-#         else:
-#             key = cv2.waitKey(0)
-#         if key == 27:
-#             return None
-#     if ok == False:
-#         return None
-#
-#     return hull_in_img
-#
+data_dir="data" # Directory that will contain all kinds of data (the data we download and the data we generate)
+
+if not os.path.isdir(data_dir):
+    os.makedirs(data_dir)
+
+card_suits=['s','h','d','c']
+card_values=['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+
+# Pickle file containing the background images from the DTD
+backgrounds_pck_fn=data_dir+"/backgrounds.pck"
+
+# Pickle file containing the card images
+cards_pck_fn=data_dir+"/cards.pck"
+
+
+# imgW,imgH: dimensions of the generated dataset images
+imgW=720
+imgH=720
+
+
+
+#nealcards
+cardW=57
+cardH=87
+cornerXmin=10
+cornerXmax=8
+cornerYmin=4
+cornerYmax=50
+
+refCard=np.array([[0,0],[cardW,0],[cardW,cardH],[0,cardH]],dtype=np.float32)
+
+refCardRot=np.array([[cardW,0],[cardW,cardH],[0,cardH],[0,0]],dtype=np.float32)
+
+refCornerHL=np.array([[cornerXmin,cornerYmin],[cornerXmax,cornerYmin],[cornerXmax,cornerYmax],[cornerXmin,cornerYmax]],dtype=np.float32)
+
+refCornerLR=np.array([[cardW-cornerXmax,cardH-cornerYmax],[cardW-cornerXmin,cardH-cornerYmax],[cardW-cornerXmin,cardH-cornerYmin],[cardW-cornerXmax,cardH-cornerYmin]],dtype=np.float32)
+
+refCorners=np.array([refCornerHL,refCornerLR])
+
+
+
+def findHull(img, corner=refCornerHL, debug="no"):
+    """
+        Find in the zone 'corner' of image 'img' and return, the convex hull delimiting
+        the value and suit symbols
+        'corner' (shape (4,2)) is an array of 4 points delimiting a rectangular zone,
+        takes one of the 2 possible values : refCornerHL or refCornerLR
+        debug=
+    """
+
+    kernel = np.ones((3, 3), np.uint8)
+    corner = corner.astype(np.int)
+
+    # We will focus on the zone of 'img' delimited by 'corner'
+    x1 = int(corner[0][0])
+    y1 = int(corner[0][1])
+    x2 = int(corner[2][0])
+    y2 = int(corner[2][1])
+    w = x2 - x1
+    h = y2 - y1
+    zone = img[y1:y2, x1:x2].copy()
+    im
+
+    strange_cnt = np.zeros_like(zone)
+    gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
+    thld = cv2.Canny(gray, 30, 200)
+    thld = cv2.dilate(thld, kernel, iterations=1)
+    if debug != "no": cv2.imshow("thld", thld)
+
+    # Find the contours
+    contours, _ = cv2.findContours(thld.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    min_area = 30  # We will reject contours with small area. TWEAK, 'zoom' dependant
+    min_solidity = 0.3  # Reject contours with a low solidity. TWEAK
+
+    concat_contour = None  # We will aggregate in 'concat_contour' the contours that we want to keep
+
+    ok = True
+    for c in contours:
+        area = cv2.contourArea(c)
+
+        hull = cv2.convexHull(c)
+        hull_area = cv2.contourArea(hull)
+        solidity = float(area) / hull_area
+        # Determine the center of gravity (cx,cy) of the contour
+        M = cv2.moments(c)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        #  abs(w/2-cx)<w*0.3 and abs(h/2-cy)<h*0.4 : TWEAK, the idea here is to keep only the contours which are closed to the center of the zone
+        if area >= min_area and abs(w / 2 - cx) < w * 0.3 and abs(h / 2 - cy) < h * 0.4 and solidity > min_solidity:
+            if debug != "no":
+                cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
+            if concat_contour is None:
+                concat_contour = c
+            else:
+                concat_contour = np.concatenate((concat_contour, c))
+        if debug != "no" and solidity <= min_solidity:
+            print("Solidity", solidity)
+            cv2.drawContours(strange_cnt, [c], 0, 255, 2)
+            cv2.imshow("Strange contours", strange_cnt)
+
+    if concat_contour is not None:
+        # At this point, we suppose that 'concat_contour' contains only the contours corresponding the value and suit symbols
+        # We can now determine the hull
+        hull = cv2.convexHull(concat_contour)
+        hull_area = cv2.contourArea(hull)
+        # If the area of the hull is to small or too big, there may be a problem
+        min_hull_area = 940  # TWEAK, deck and 'zoom' dependant
+        max_hull_area = 2120  # TWEAK, deck and 'zoom' dependant
+        if hull_area < min_hull_area or hull_area > max_hull_area:
+            ok = False
+            if debug != "no":
+                print("Hull area=", hull_area, "too large or too small")
+        # So far, the coordinates of the hull are relative to 'zone'
+        # We need the coordinates relative to the image -> 'hull_in_img'
+        hull_in_img = hull + corner[0]
+
+    else:
+        ok = False
+
+    if debug != "no":
+        if concat_contour is not None:
+            cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
+            cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
+        cv2.imshow("Zone", zone)
+        cv2.imshow("Image", img)
+        if ok and debug != "pause_always":
+            key = cv2.waitKey(1)
+        else:
+            key = cv2.waitKey(0)
+        if key == 27:
+            return None
+    if ok == False:
+        return None
+
+    return hull_in_img
+
 
 
 
