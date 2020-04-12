d5f83c77726b554b974285516468ec1ee7efd8db
kalkaran <neal.norman@gmail.com>
2020-04-12 13:20:44 +0200

    Finding a corner - issue seems to be the area is completely wrong

--- testbed.py
+++ testbed.py
@@ -713,13 +713,38 @@ refCorners=np.array([refCornerHL,refCornerLR])
 
 
 
-def findHull(img, corner=refCornerHL, debug="no"):
+#looks like the the corner it is finding is way off.
+def findHull_imageAnalysis(img, corner):
+    kernel = np.ones((3, 3), np.uint8)
+    corner = corner.astype(np.int)
+
+    # We will focus on the zone of 'img' delimited by 'corner'
+    x1 = int(corner[0][0])
+    y1 = int(corner[0][1])
+    x2 = int(corner[2][0])
+    y2 = int(corner[2][1])
+    w = x2 - x1
+    h = y2 - y1
+    zone = img[y1:y2, x1:x2].copy()
+
+    return zone
+
+imghull = cv2.imread("./data/cards/2c/2c.jpg")
+print(type(imghull))
+# cv2.imshow("preimg",imghull)
+# cv2.waitKey(0)
+cv2.imshow("Image Corner Analysis", findHull_imageAnalysis(imghull, refCornerLR))
+cv2.waitKey(0)
+
+
+
+def findHull(img, corner=refCornerHL):
     """
+        this function is taken from Jupyternotebook.
         Find in the zone 'corner' of image 'img' and return, the convex hull delimiting
         the value and suit symbols
         'corner' (shape (4,2)) is an array of 4 points delimiting a rectangular zone,
         takes one of the 2 possible values : refCornerHL or refCornerLR
-        debug=
     """
 
     kernel = np.ones((3, 3), np.uint8)
@@ -739,7 +764,9 @@ def findHull(img, corner=refCornerHL, debug="no"):
     gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
     thld = cv2.Canny(gray, 30, 200)
     thld = cv2.dilate(thld, kernel, iterations=1)
-    if debug != "no": cv2.imshow("thld", thld)
+
+    cv2.imshow("handled", thld)
+    cv2.waitKey(0)
 
     # Find the contours
     contours, _ = cv2.findContours(thld.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -762,16 +789,16 @@ def findHull(img, corner=refCornerHL, debug="no"):
         cy = int(M['m01'] / M['m00'])
         #  abs(w/2-cx)<w*0.3 and abs(h/2-cy)<h*0.4 : TWEAK, the idea here is to keep only the contours which are closed to the center of the zone
         if area >= min_area and abs(w / 2 - cx) < w * 0.3 and abs(h / 2 - cy) < h * 0.4 and solidity > min_solidity:
-            if debug != "no":
-                cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
+            # if debug != "no":
+            #     cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
             if concat_contour is None:
                 concat_contour = c
             else:
                 concat_contour = np.concatenate((concat_contour, c))
-        if debug != "no" and solidity <= min_solidity:
-            print("Solidity", solidity)
-            cv2.drawContours(strange_cnt, [c], 0, 255, 2)
-            cv2.imshow("Strange contours", strange_cnt)
+        # if debug != "no" and solidity <= min_solidity:
+        #     print("Solidity", solidity)
+        #     cv2.drawContours(strange_cnt, [c], 0, 255, 2)
+        #     cv2.imshow("Strange contours", strange_cnt)
 
     if concat_contour is not None:
         # At this point, we suppose that 'concat_contour' contains only the contours corresponding the value and suit symbols
@@ -783,8 +810,8 @@ def findHull(img, corner=refCornerHL, debug="no"):
         max_hull_area = 2120  # TWEAK, deck and 'zoom' dependant
         if hull_area < min_hull_area or hull_area > max_hull_area:
             ok = False
-            if debug != "no":
-                print("Hull area=", hull_area, "too large or too small")
+            # if debug != "no":
+            #     print("Hull area=", hull_area, "too large or too small")
         # So far, the coordinates of the hull are relative to 'zone'
         # We need the coordinates relative to the image -> 'hull_in_img'
         hull_in_img = hull + corner[0]
@@ -792,20 +819,20 @@ def findHull(img, corner=refCornerHL, debug="no"):
     else:
         ok = False
 
-    if debug != "no":
-        if concat_contour is not None:
-            cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
-            cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
-        cv2.imshow("Zone", zone)
-        cv2.imshow("Image", img)
-        if ok and debug != "pause_always":
-            key = cv2.waitKey(1)
-        else:
-            key = cv2.waitKey(0)
-        if key == 27:
-            return None
-    if ok == False:
-        return None
+    # if debug != "no":
+    #     if concat_contour is not None:
+    #         cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
+    #         cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
+    #     cv2.imshow("Zone", zone)
+    #     cv2.imshow("Image", img)
+    #     if ok and debug != "pause_always":
+    #         key = cv2.waitKey(1)
+    #     else:
+    #         key = cv2.waitKey(0)
+    #     if key == 27:
+    #         return None
+    # if ok == False:
+    #     return None
 
     return hull_in_img
 
@@ -814,6 +841,11 @@ def findHull(img, corner=refCornerHL, debug="no"):
 def main():
     #to extra cards from dataset
     #extract_all()
+    #to find hulls. - requires cards to be extracted.
+    imghull = cv2.imread("./data/cards/2c/2c.jpg")
+    #findHull(imghull)
+
+
 
 
 
